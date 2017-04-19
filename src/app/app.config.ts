391422bf2628c4ee44673ea6a6e787ebd9de72d3
ericb <eric@hellouser.net>
2017-04-19 07:55:14 -0500

    declare web client

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,7 @@ declare const aws_mobile_analytics_app_id;
 declare const aws_cognito_region;
 declare const aws_cognito_identity_pool_id;
 declare const aws_user_pools_id;
-declare const aws_user_pools_client_id;
+declare const aws_user_pools_web_client_id;
 declare const aws_user_files_s3_bucket;
 
 @Injectable()
